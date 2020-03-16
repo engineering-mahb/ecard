816448ee3d8727132703953163ee07b281f5c47b
Redha Azmei <redha.azmei@gmail.com>
2020-03-17 02:49:55 +0800

    Modified html contents and Datatables Javascript.

--- js/datatables-demo.js
+++ js/datatables-demo.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-  $('#dataTable').DataTable( {
-      dom: 'Bfrtip',
-      buttons: [
-          'excel', 'pdf', 'print'
-      ]
+  var table = $('#dataTable').DataTable( {
+      order: [[ 1, 'asc' ], [ 2, 'asc' ]],
+      scrollY: '50vh',
+      scrollCollapse: true,
+      paging: false,
+    //   lengthChange: true,
+    //   lengthMenu: [10,25,50],
+      buttons: [ 'excel', 'pdf']
   } );
+
+  table.buttons().container()
+      .appendTo( '#dataTable_wrapper .col-md-6:eq(0)' );
 } );
